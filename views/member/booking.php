76bc2393daab4618dc670db7484a3f1da7402f9c
jxxxuan <87358766+jxxxuan@users.noreply.github.com>
2023-05-18 23:46:41 +0800

    jx

--- views/member/booking.php
+++ views/member/booking.php
@@ -47,8 +47,9 @@
 			$bookings = $db->table('booking')->where('maid_id',$_SESSION['maid_id'])->rows();
 			$booked_time = array();
 			foreach ($bookings as $booking) {
-				$booked_time[] = date("H", strtotime($booking['booking_arrive_time']));
+				$booked_time[] = date('Y-m-d H',strtotime($booking['booking_arrive_time']));
 			}
+			echo $booked_time[0];
 	?>
 			<table class='table-container'>
 				<thead>
@@ -79,50 +80,43 @@
 						// Set the timezone to your local timezone
 						date_default_timezone_set('Asia/Kuala_Lumpur');
 
-						$date = '2023-5-30';
+						$current_date = date('Y-m-d H:i');
 						// Get the current week's start and end date
-						$current_week_start = strtotime('monday this week',strtotime($date));
-						$current_week_end = strtotime('sunday this week',strtotime($date));
-					
-						// Define the start and end time for the time slots
-						$start_time = strtotime('8:00');
-						$end_time = strtotime('18:00');
+						$current_week_start = strtotime('monday this week', strtotime($current_date));
+						$current_week_end = strtotime('sunday this week', strtotime($current_date));
 					?>
+
 					<thead>
 						<tr>
 							<th>Time</th>
-							<th>Monday <?php echo '<br>'.date('Y-m-d', strtotime('+0 days',$current_week_start));?></th>
-							<th>Tuesday <?php echo '<br>'.date('Y-m-d', strtotime('+1 days',$current_week_start));?></th>
-							<th>Wednesday <?php echo '<br>'.date('Y-m-d', strtotime('+2 days',$current_week_start));?></th>
-							<th>Thursday <?php echo '<br>'.date('Y-m-d', strtotime('+3 days',$current_week_start));?></th>
-							<th>Friday <?php echo '<br>'.date('Y-m-d', strtotime('+4 days',$current_week_start));?></th>
-							<th>Saturday <?php echo '<br>'.date('Y-m-d', strtotime('+5 days',$current_week_start));?></th>
-							<th>Sunday <?php echo '<br>'.date('Y-m-d', strtotime('+6 days',$current_week_start));?></th>
+							<th>Monday <?php echo '<br>' . date('Y-m-d', $current_week_start); ?></th>
+							<th>Tuesday <?php echo '<br>' . date('Y-m-d', strtotime('+1 day', $current_week_start)); ?></th>
+							<th>Wednesday <?php echo '<br>' . date('Y-m-d', strtotime('+2 days', $current_week_start)); ?></th>
+							<th>Thursday <?php echo '<br>' . date('Y-m-d', strtotime('+3 days', $current_week_start)); ?></th>
+							<th>Friday <?php echo '<br>' . date('Y-m-d', strtotime('+4 days', $current_week_start)); ?></th>
+							<th>Saturday <?php echo '<br>' . date('Y-m-d', strtotime('+5 days', $current_week_start)); ?></th>
+							<th>Sunday <?php echo '<br>' . date('Y-m-d', strtotime('+6 days', $current_week_start)); ?></th>
 						</tr>
 					</thead>
 					<tbody>
-					
-					<?php
-						echo $current_week_start;
-						echo $current_week_end;
-						/*
-						for($days = $current_week_start;$days < $current_week_end;strtotime('+1 days',$current_week_start)){
-							
-							for ($i = $start_time; $i <= $end_time; $i += 3600) {
+						<?php
+							for ($i = strtotime('8:00'); $i <= strtotime('18:00'); $i += 3600) {
 								echo "<tr>";
-								echo "<td>".date('H:i', $i)."</td>";
-								
-									if(in_array(date('H', $i),$booked_time)){
-										echo "<td><button class='button time-slot-button not-available' onclick='select(this)'>".date('H', $i)."</td>";
-									}else{
-										echo "<td><button class='button time-slot-button' onclick='select(this)'>".date('H', $i)."</td>";
+								echo "<td>" . date('H:i', $i) . "</td>";
+								for ($days = $current_week_start; $days <= $current_week_end; $days = strtotime('+1 day', $days)) {
+									$dayDate = date('Y-m-d', $days);
+									$dateTime = strtotime($dayDate . ' ' . date('H:i', $i));
+									$buttonClass = '';
+									if (in_array(date('Y-m-d H',$dateTime), $booked_time)) {
+										$buttonClass = 'not-available';
+									} else if(strtotime(date('Y-m-d H:i',$dateTime)) < strtotime($current_date)){
+										$buttonClass = 'passed';
 									}
-								
+									echo "<td><button class='button time-slot-button $buttonClass' onclick='select(this)'>" . date('H', $i) . "</td>";
+								}
+								echo "</tr>";
 							}
-							echo "</tr>";
-							
-						}*/
-					?>
+						?>
 					</tbody>
 				</table>
 			</section>
@@ -133,10 +127,21 @@
 	<a href="#" class="booking-button button">Cancel Booking</a>
 </div>
 <script>
-	function select(button){
+	function select(button) {
 		button.classList.toggle('selected');
+		if check_valid(button){
+			
+		}
 	}
-	function check_valid(button){
-		
+
+	function check_valid(button) {
+		if (button.classList.contains('not-available') or button.classList.contains('passed')) {
+			// Button is not available
+			return False;
+		} else {
+			// Button is available
+			return True;
+		}
 	}
+
 </script>
