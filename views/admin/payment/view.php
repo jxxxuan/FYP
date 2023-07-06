e03ae17eb8b98f299f80597976d4ee2d140900f4
jxxxuan <87358766+jxxxuan@users.noreply.github.com>
2023-07-07 00:48:29 +0800

    jx

--- views/admin/payment/view.php
+++ views/admin/payment/view.php
@@ -4,51 +4,24 @@ if (!authenticated(ADMIN_ROLE)) {
 }
 
 $db = new Database();
-$bookings = $db->table('booking')->rows();
+$payments = $db->table('payment')->rows();
 $flash = getFlash('message');
-
-if (isPostMethod() && isset($_POST['status'])){
-	$status = $_POST['status'];
-	$bookings = $db->table('booking')
-		-> Where('booking_status',$status)
-		-> rows();
-} else {
-	$bookings = $db-> table('booking')
-		-> rows();
-}	
-?>
-
-<form class='box' action="" method="post">
-	<div style='text-align:left'>
-		<label for="status">booking status: </label>
-		<select name="status" id="status">
-			<option value="Pending" <?php if (isset($_POST['status']) && $_POST['status'] === 'Pending') echo 'selected'; ?>>Pending</option>
-			<option value="Reject" <?php if (isset($_POST['status']) && $_POST['status'] === 'Reject') echo 'selected'; ?>>Reject</option>
-			<option value="Confirm" <?php if (isset($_POST['status']) && $_POST['status'] === 'Confirm') echo 'selected'; ?>>Confirm</option>
-			<option value="Working" <?php if (isset($_POST['status']) && $_POST['status'] === 'Working') echo 'selected'; ?>>Working</option>
-			<option value="Completed" <?php if (isset($_POST['status']) && $_POST['status'] === 'Completed') echo 'selected'; ?>>Completed</option>
-		</select>
-
-		<button type="submit">Filter</button>
-	</div>
-</form>
 
 <?php
 if(count($bookings) > 0){
 ?>
-	<h2>BOOKING LIST</h2>
+	<h2>PAYMENT LIST</h2>
 	<table class="admin-table box">
 		<thead>
 			<tr>
+				<th>Payment ID</th>
 				<th>Booking ID</th>
-				<th>Service ID</th>
 				<th>Member ID</th>
-				<th>Maid ID</th>
-				<th>Booking Time</th>
-				<th>Booking Status</th>
-				<th>Booking Arrive Time</th>
-				<th>Booking Address</th>
-				<th>Booking Leave Time</th>
+				<th>Payemnt type</th>
+				<th>Payemnt price</th>
+				<th>Bank name</th>
+				<th>Bank card holder name</th>
+				<th>Bank card number</th>
 			</tr>
 		</thead>
 
@@ -56,9 +29,9 @@ if(count($bookings) > 0){
 			<?php foreach ($bookings as $booking) { ?>
 				<tr>
 					<td><?php echo $booking['booking_id']; ?></td>
-					<td><a href=<?php echo route('service/service',$booking['service_id'])?>><?php echo $booking['service_id']; ?></a></td>
+					<td><?php echo $booking['booking_datetime']; ?></td>
+					<td><?php echo $booking['booking_datetime']; ?></td>
 					<td><a href=<?php echo route('member/member_profile',$booking['member_id'])?>><?php echo $booking['member_id']; ?></a></td>
-					<td><a href=<?php echo route('maid/maid_profile?maid_id='.$booking['maid_id'])?>><?php echo $booking['maid_id']; ?></a></td>
 					<td><?php echo $booking['booking_datetime']; ?></td>
 					<td><?php echo $booking['booking_status']; ?></td>
 					<td><?php echo $booking['booking_arrive_datetime']; ?></td>
