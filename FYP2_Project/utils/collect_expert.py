55ebabb0f4fb1c20c11f7fbc79d2db6fe020c293
jxxxuan <87358766+jxxxuan@users.noreply.github.com>
2026-04-13 17:11:21 +0800

    Update collect_expert.py

--- FYP2_Project/utils/collect_expert.py
+++ FYP2_Project/utils/collect_expert.py
@@ -133,7 +133,7 @@ def collect_data_from_json(json_path, target_town="Town03"):
 if __name__ == "__main__":
     # 确保当前路径有 tasks.json
     try:
-        collect_data_from_json(TRAIN_JSON, target_town="Town04")
+        collect_data_from_json(TRAIN_JSON, target_town="Town05")
         # collect_single_task(TRAIN_JSON, target_town="Town05", target_id="25")
     except Exception as e:
         import traceback
