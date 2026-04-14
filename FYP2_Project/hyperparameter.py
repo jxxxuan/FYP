67afd6affaa929d7799a9345a784f221ceebae44
jxxxuan <87358766+jxxxuan@users.noreply.github.com>
2026-04-14 16:19:45 +0800

    4/14

--- FYP2_Project/hyperparameter.py
+++ FYP2_Project/hyperparameter.py
@@ -1,7 +1,7 @@
 LR = 3e-4
 GAMMA = 0.99
-BATCH_SIZE_A = 128  # 智能体批大小
-BATCH_SIZE_E = 128  # 专家批大小
+BATCH_SIZE_A = 64  # 智能体批大小
+BATCH_SIZE_E = 64  # 专家批大小
 TAU = 0.005        # 软更新系数
 ALPHA = 0.2        # 初始温度参数
 NUM_NPC = 40
