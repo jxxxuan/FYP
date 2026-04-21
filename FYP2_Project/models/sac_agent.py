1922ec463661969bee1ac23b2fc5930b9ee85809
jxxxuan <87358766+jxxxuan@users.noreply.github.com>
2026-04-21 13:00:33 +0800

    Update sac_agent.py

--- FYP2_Project/models/sac_agent.py
+++ FYP2_Project/models/sac_agent.py
@@ -107,7 +107,7 @@ class ObsBuffer:
         if action is not None: self.action_pool.append(action)
         if reward is not None: self.reward_pool.append(reward)
         if debug_frame is not None: self.debug_frame_pool.append(debug_frame)
-        if debug_frame == self.debug_frame_pool[len(self.debug_frame_pool)-1]:
+        if debug_frame == self.debug_frame_pool[len(self.debug_frame_pool)-2]:
             print('same with last img')
         self.ptr += 1
 
