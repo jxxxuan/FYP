2e5508b25ebddad45c73dfd37324fa606c10a5f4
jxxxuan <87358766+jxxxuan@users.noreply.github.com>
2026-05-09 10:16:19 +0800

    Update start_carla.py

--- FYP_Project/start_carla.py
+++ FYP_Project/start_carla.py
@@ -4,9 +4,9 @@ import time
 def restart_carla_docker(container_name="carla_server"):
     print(f"--- 正在重启 Docker 容器: {container_name} ---")
     try:
-        # docker run --privileged --runtime nvidia --name carla-server --rm --net=host 
-        # --gpus all carlasim/carla:0.9.15 ./CarlaUE4.sh -RenderOffScreen -nosound -quality-level=High 
-        # -ResX=1 -ResY=1 -fps=5 -carla-port=2000
+        # docker run --privileged --runtime nvidia --name carla-server --rm --net=host --gpus all 
+        # carlasim/carla:0.9.15 ./CarlaUE4.sh -RenderOffScreen -nosound -quality-level=High -ResX=1 -ResY=1 -fps=5 
+        # -carla-port=2000
         # 1. 停止并移除现有容器（强制释放显存和端口）
         subprocess.run(["docker", "stop", container_name], check=False)
         subprocess.run(["docker", "rm", "-f", container_name], check=False)
@@ -14,19 +14,17 @@ def restart_carla_docker(container_name="carla_server"):
         # 2. 重新启动容器
         # 注意：这里需要根据你的镜像版本和显卡配置修改参数
         docker_cmd = [
-            "docker", "run", "-d", 
+            "docker", "run", 
             "--privileged",
+            "--runtime","nvidia",
             "--name", container_name,
-            "--gpus", "all",             # 使用 GPU
-            "-p", "2000-2002:2000-2002", # 映射端口
-            "--runtime=nvidia",
-            "-e", "NVIDIA_VISIBLE_DEVICES=all",
+            "--net=host",
+            "--gpus", "all",  
             "carlasim/carla:0.9.15",     # 你的镜像名称
-            "/bin/bash", "CarlaUE4.sh", 
-            "-RenderOffScreen",                 # 必须添加：禁止渲染窗口
-            "-nosound",                         # 建议添加：禁止音频，节省资源
-            "-quality-level=Low", 
-            "-vulkan"
+            "./CarlaUE4.sh", 
+            "-RenderOffScreen", "-nosound",# 建议添加：禁止音频，节省资源
+            "-quality-level=High", 
+            "-ResX=1", "-ResY=1", "-fps=5",
         ]
         subprocess.run(docker_cmd, check=True)
         
