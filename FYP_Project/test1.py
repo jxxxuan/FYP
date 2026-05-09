a5af7a3405750f8a908171ae558e9ad462df7347
jxxxuan <87358766+jxxxuan@users.noreply.github.com>
2026-05-09 16:55:40 +0800

    Update test1.py

--- FYP_Project/test1.py
+++ FYP_Project/test1.py
@@ -2,13 +2,26 @@ import carla
 from constants import *
 import time
 
+# 1. 加载地图
 client = carla.Client(CARLA_HOST, int(CARLA_PORT))
 client.set_timeout(10.0)
-client.load_world("Town03_Opt", carla.MapLayer.All)
-print('loaded1')
-time.sleep(10)
-tm = client.get_trafficmanager()
-tm.shut_down()
-print('got tm')
+client.load_world("Town03_Opt")
+world = client.get_world()
+print('Map command sent')
+
+time.sleep(5) 
+
+# 3. 开启世界同步，但不开启 TM 同步
+settings = world.get_settings()
+settings.synchronous_mode = True
+world.apply_settings(settings)
+
+# 4. 手动 Tick 几次，让物理引擎和地图数据对齐
+for _ in range(20):
+    world.tick()
+print('World stabilized')
+
+# 5. 最后再启动 TM
+tm = client.get_trafficmanager(8000)
 tm.set_synchronous_mode(True)
-print('loaded2')
+print('TM successfully joined!')
