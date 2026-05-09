f9d58b4c6b5540ad21f2e6d7bbd22c3a4d158a4c
jxxxuan <87358766+jxxxuan@users.noreply.github.com>
2026-05-09 14:48:34 +0800

    Update test1.py

--- FYP_Project/test1.py
+++ FYP_Project/test1.py
@@ -3,6 +3,10 @@ from constants import *
 
 client = carla.Client(CARLA_HOST, int(CARLA_PORT))
 client.set_timeout(10.0)
+# tm = client.get_trafficmanager(8000)
+# tm.set_synchronous_mode(True)
+client.load_world("Town03_Opt")
+print('loaded1')
 tm = client.get_trafficmanager(8000)
 tm.set_synchronous_mode(True)
-client.load_world("Town03_Opt")
+print('loaded2')
