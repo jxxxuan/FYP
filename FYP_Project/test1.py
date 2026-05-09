33007f8188a768db8d6d376f88ab2c91aaee1277
jxxxuan <87358766+jxxxuan@users.noreply.github.com>
2026-05-09 14:58:47 +0800

    Update test1.py

--- FYP_Project/test1.py
+++ FYP_Project/test1.py
@@ -1,13 +1,16 @@
 import carla
 from constants import *
+import time
+
 
 client = carla.Client(CARLA_HOST, int(CARLA_PORT))
 client.set_timeout(10.0)
 # tm = client.get_trafficmanager(8000)
 # tm.set_synchronous_mode(True)
-client.load_world("Town03_Opt")
+client.load_world("Town03_Opt", carla.MapLayer.Buildings | carla.MapLayer.ParkedVehicles)
 print('loaded1')
-tm = client.get_trafficmanager(8100)
+time.sleep(10)
+tm = client.get_trafficmanager(8000)
 print('got tm')
 tm.set_synchronous_mode(True)
 print('loaded2')
