c895bf37f020af44eea5ae49471ba7d8161c33bd
jxxxuan <87358766+jxxxuan@users.noreply.github.com>
2026-05-09 13:04:11 +0800

    5/9

--- FYP_Project/test.py
+++ FYP_Project/test.py
@@ -1,20 +1,52 @@
-import carla
+import random
+import torch
+import os
+from hyperparameter import *
 from constants import *
-from agents.navigation.global_route_planner import GlobalRoutePlanner
+from utils.utils import *
+from bc import *
+from start_carla import restart_carla_docker
 
-def _connect_to_carla(self):
-    self.tm = self.client.get_trafficmanager(8000)
-    self.tm.set_synchronous_mode(True)
+@torch.no_grad()
+def test(env, target_town, tasks, junctions, actor, current_episode, writer):
+    tasks_in_town = tasks.get(target_town, [])
+    if not tasks_in_town:
+        return
     
-def _load_world(self, town):
-    settings = self.world.get_settings()
-    settings.synchronous_mode = True
-    settings.fixed_delta_seconds = FIXED_DELTA_SECONDS     
-    settings.max_substep_delta_time = SUBSTEP_DELTA
-    settings.max_substeps = MAX_SUBSTEPS
-    self.world.apply_settings(settings)
-    self.map = self.world.get_map()
-    self.grp = GlobalRoutePlanner(self.map, GRP)
+    selected_task = random.choice(tasks_in_town)
+    junction_name = selected_task['junction_name']
+    junction_data = junctions[target_town].get('test_junctions', {}).get(junction_name, [])
+
+    actual_actor = actor._orig_mod if hasattr(actor, "_orig_mod") else actor
+    actual_actor.eval()
+    
+    video_path = os.path.join(RC_DIR, f"debug_{target_town}_ep{current_episode}.mp4")
+    start, target = build_pose(selected_task)
+
+    obs, _ = env.reset(target_town, level=0, junction_data=junction_data, start_transform=start, target_location=target, video_path=video_path)
+    
+    episode_reward = 0
+    done = False
+    step = 0
+    
+    while step < MAX_STEPS + (MAX_STEPS * 0.2) and not done:
+        v_input, g_input = preprocess_obs(obs['visual'], obs['goal'], device)
+
+        mu, _ = actual_actor(v_input, g_input)
+        action = torch.tanh(mu)
+        action_numpy = action.detach().cpu().numpy()[0]
+
+        next_obs, reward, terminated, truncated, info = env.step(action_numpy)
+        
+        obs = next_obs
+        episode_reward += reward
+        step += 1
+        done = terminated or truncated
+            
+    print(f"Test Run Reward: {episode_reward:.2f} | Steps: {step} | Reason: {info['reason']}")
+    writer.add_scalar('Reward/Test', episode_reward, current_episode)
+
+    actual_actor.train()
 
 if __name__ == '__main__':
     for i in range(100):
--- FYP_Project/train.py
+++ FYP_Project/train.py
@@ -1,7 +1,4 @@
-from collections import deque
 import gc
-import random
-import numpy as np
 import torch
 from envs.carla_env import CarlaEnv
 from models.sac_agent import MixedReplayBuffer
@@ -125,47 +122,6 @@ def soft_update(net, target_net, tau):
             tau * param.data + (1.0 - tau) * target_param.data
         )
 
-@torch.no_grad()
-def test(env, target_town, tasks, junctions, actor, current_episode, writer):
-    tasks_in_town = tasks.get(target_town, [])
-    if not tasks_in_town:
-        return
-    
-    selected_task = random.choice(tasks_in_town)
-    junction_name = selected_task['junction_name']
-    junction_data = junctions[target_town].get('test_junctions', {}).get(junction_name, [])
-
-    actual_actor = actor._orig_mod if hasattr(actor, "_orig_mod") else actor
-    actual_actor.eval()
-    
-    video_path = os.path.join(RC_DIR, f"debug_{target_town}_ep{current_episode}.mp4")
-    start, target = build_pose(selected_task)
-
-    obs, _ = env.reset(target_town, level=0, junction_data=junction_data, start_transform=start, target_location=target, video_path=video_path)
-    
-    episode_reward = 0
-    done = False
-    step = 0
-    
-    while step < MAX_STEPS + (MAX_STEPS * 0.2) and not done:
-        v_input, g_input = preprocess_obs(obs['visual'], obs['goal'], device)
-
-        mu, _ = actual_actor(v_input, g_input)
-        action = torch.tanh(mu)
-        action_numpy = action.detach().cpu().numpy()[0]
-
-        next_obs, reward, terminated, truncated, info = env.step(action_numpy)
-        
-        obs = next_obs
-        episode_reward += reward
-        step += 1
-        done = terminated or truncated
-            
-    print(f"Test Run Reward: {episode_reward:.2f} | Steps: {step} | Reason: {info['reason']}")
-    writer.add_scalar('Reward/Test', episode_reward, current_episode)
-
-    actual_actor.train()
-
 if __name__ == '__main__':
     # restart_carla_docker()
     env = CarlaEnv()
@@ -236,18 +192,18 @@ if __name__ == '__main__':
 
             if current_episode % CHECK_POINT_INTERVAL == 0:
                 save_checkpoint(actor, actor_opt, critic, critic_opt, alpha_opt, log_alpha, current_episode, models['global_step'])
-                test(env, target_town="Town04", tasks=test_tasks, junctions=junctions, actor=actor, current_episode=current_episode, writer=writer)
+                # test(env, target_town="Town04", tasks=test_tasks, junctions=junctions, actor=actor, current_episode=current_episode, writer=writer)
                 # env.close()
                 # restart_carla_docker()
-                for i in range(5):
-                    try:
-                        env = CarlaEnv()
-                        break
-                    except Exception as e:
-                        print(f"CarlaEnv init failed ({i+1}/5): {e}")
-                        time.sleep(5)
-                else:
-                    raise RuntimeError("Cannot reconnect to CARLA")
+                # for i in range(5):
+                #     try:
+                #         env = CarlaEnv()
+                #         break
+                #     except Exception as e:
+                #         print(f"CarlaEnv init failed ({i+1}/5): {e}")
+                #         time.sleep(5)
+                # else:
+                #     raise RuntimeError("Cannot reconnect to CARLA")
 
     except KeyboardInterrupt:
         print("\n[DETECTED] Ctrl+C")
