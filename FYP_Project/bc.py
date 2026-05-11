6a00955f25c0da72cef68ffd53212d4dcc363e9e
jxxxuan <87358766+jxxxuan@users.noreply.github.com>
2026-05-11 14:42:50 +0800

    Update bc.py

--- FYP_Project/bc.py
+++ FYP_Project/bc.py
@@ -4,46 +4,24 @@ from constants import *
 from utils.utils import *
 
 # 伪代码：在 train() 函数开头或外部进行
-def behavioral_cloning_pretrain(actor, actor_opt, writer, buffer, iterations=2000):
+# def behavioral_cloning_pretrain(actor, opt, writer, buffer, iterations=2000):
+def behavioral_cloning_pretrain(model, opt, writer, buffer, iterations=2000):
     print("--- Starting Behavioral Cloning Pre-training (Town04/05) ---")
-    actor.train()
+    # actor.train()
+    model.train()
 
     for i in range(iterations):
         # 从 Town04, Town05 的 Buffer 中采样专家经验
         # 这里采样的是训练集数据
         e_s, e_a, _, _, _ = buffer.sample_expert(BC_BATCH_SIZE)
         
-        pred_mu, _ = actor(e_s['visual'], e_s['goal'])
+        # pred_mu, _ = actor(e_s['visual'], e_s['goal'])
+        feat = model.get_feature(e_s['visual'])
+        pred_mu, _ = model.actor(feat, e_s['goal'])
         train_loss = F.mse_loss(torch.tanh(pred_mu), e_a)
         
-        actor_opt.zero_grad()
+        opt.zero_grad()
         train_loss.backward()
-        actor_opt.step()
+        opt.step()
 
         writer.add_scalar('BC/Train Loss', train_loss.item(), i)
-    
-@torch.no_grad()
-@torch.no_grad()
-def validate(actor, val_data):
-    """
-    使用一次性加载的 Town03 数据进行验证
-    """
-    if val_data is None:
-        return 0.0
-    
-    actor.eval()
-    
-    # 直接获取全量验证数据
-    v = val_data['state']['visual'] # 已经是 float 且归一化过的
-    g = val_data['state']['goal']
-    expert_a = val_data['action']
-    
-    # 前向传播 (如果显存压力大，这里可以分批跑，但 Town03 一般没问题)
-    pred_mu, _ = actor(v, g)
-    
-    # 计算 MSE Loss
-    # 注意：论文中输出通常经过 tanh 限制在 [-1, 1]
-    loss = F.mse_loss(torch.tanh(pred_mu), expert_a)
-    
-    actor.train()
-    return loss.item()
